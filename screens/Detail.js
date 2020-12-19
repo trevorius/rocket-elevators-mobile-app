c7537c4d96f7e3419dfc2efa69ca3b9df7322aaf
trevorius <trevorius45@hotmail.com>
2020-12-19 15:27:38 -0500

    created screens

---
+++ screens/Detail.js
@@ -0,0 +1,30 @@
+// src/screens/Home.js
+
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Details Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ebebeb'
+  },
+  text: {
+    color: '#101010',
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
+
+export default Home
+
+// src/screens/Detail.js
