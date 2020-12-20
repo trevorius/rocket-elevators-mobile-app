471b0f1ffa7151acab98074babe6833139b77649
trevorius <trevorius45@hotmail.com>
2020-12-20 11:37:15 -0500

    commented code

--- src/logic/HomeLogic.js
+++ src/logic/HomeLogic.js
@@ -7,30 +7,30 @@ const fetch = require('node-fetch')
 
 
 
-
+// a general function to return a list of inactive elevators
 async function getInactiveElevators(){
-    
+    // set the endpoint that will be called 
     const url = global.url + '/api/elevators/not-operating';
     let elevatorDisplay = [];
-    console.log("testing")
-    console.log(url)
+    // make the api call
     const response = await fetch(url,
         {
             "method":"GET"
         })
     try{
+        // jsonify the response
         const inactiveElevators = await response.json();
-        const testElevator = inactiveElevators[3].serial_number
-        console.log("test Elevator : " + testElevator)
-
+        // populate Display array
         for (let i = 0 ; i < inactiveElevators.length; i++){
+            // extract desired information
             const elevatorInfo = {
                 serialNumber: inactiveElevators[i].serial_number,
                 ID: inactiveElevators[i].id
             }
+            // add to the array
             elevatorDisplay.push(elevatorInfo);
         }
-        console.log (elevatorDisplay);
+        // a log for the console to display the result works   
         console.log ("number of inactive elevators : " + elevatorDisplay.length);
         return elevatorDisplay;
 
